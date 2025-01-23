a5f08362bcb6682fa855e17afee8975f75478daa
helen-mark <helen-markova@yandex.ru>
2025-01-23 13:49:46 +0300

    train dsm (survival) model with our data with pseudo event times

--- dsm_test.py
+++ dsm_test.py
@@ -1,16 +1,41 @@
 from auton_survival import estimators, datasets
 from auton_survival.preprocessing import Preprocessor
+from pandas import read_csv
 
 
-def fit_survival_machine():
+def fit_survival_machine(features, outcomes):
     model = estimators.SurvivalModel(model='dsm')
     print(model)
-    features, outcomes = datasets.load_dataset("SUPPORT")
-    print(outcomes)
-    #features = Preprocessor().fit_transform(features,
-    #                                        cat_feats=['GENDER', 'ETHNICITY', 'SMOKE'],
-    #                                        num_feats=['height', 'weight'])
+    features = Preprocessor().fit_transform(features,
+                                            cat_feats=['department', 'nationality', 'gender', 'family_status'],
+                                            num_feats=[
+                    "seniority",
+                    "age",
+                    "days_before_salary_increase",
+                    "salary_increase",
+                    "overtime",
+                    "salary_6m_average",
+                    "salary_cur"])
+    print(features)
+    print(outcomes.event)
+
+    model.fit(features, outcomes)
+    predictions = model.predict_risk(features, times=[8, 12, 16])
+    print(predictions)
 
 
-if __name__=='__main__':
-    fit_survival_machine()
+def prepare_dataset(_data_path: str):
+    dataset = read_csv(config["data_path"], delimiter=',')
+    dataset = dataset.transpose()
+    feats = dataset[:-2].transpose()
+    outs = dataset[-2:].transpose()
+    return feats, outs
+
+
+if __name__ == '__main__':
+    config = {
+        "data_path": "data/dataset-nn-small-dsm.csv"  # dataset contains columns "event" and "time"
+    }
+
+    features, outcomes = prepare_dataset(config["data_path"])
+    fit_survival_machine(features, outcomes)

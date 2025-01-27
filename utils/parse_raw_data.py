8149c1290c0ce3b6a9478006c93992169c8e76a5
helen-mark <helen-markova@yandex.ru>
2025-01-27 18:11:43 +0300

    start fill dataset by columns

--- utils/parse_raw_data.py
+++ utils/parse_raw_data.py
@@ -8,10 +8,6 @@ def check_required_fields():
     pass
 
 
-def fill_basic_fields():
-    pass
-
-
 def calc_age():
     pass
 
@@ -88,25 +84,72 @@ def fill_snapshot_specific():
     pass
 
 
-def fill_common_features():
+def fill_common_features(_f_name, _dataset, _col):
+
+    _dataset.insert(len(_dataset.columns), _f_name, _col)
     pass
 
 
-def check_and_parse(_config):
-    data_dir = _config['data_location']['data_path']
-    filename = _config['data_location']['file_name']
-    file_df = read_excel(os.path.join(data_dir, filename))
-    print(file_df)
+def check_and_parse(_data_config, _dataset_config):
+    data_dir = _data_config['data_location']['data_path']
+    filename = _data_config['data_location']['file_name']
 
-    dataset = pd.DataFrame()
+    input_df = read_excel(os.path.join(data_dir, filename), sheet_name='Основные данные')
+    main_features = _data_config['required_sheets']['basic']['features']
+    common_features = {}
+
+    # collect feature input names which are common for all snapshots:
+    for f_name in main_features.keys():
+        f = main_features[f_name]
+        if 'kind' in f.keys() and f['kind'] == 'common':
+            common_features[f['name']] = f['name_out']
+    print(f'common: {common_features}\n')
+
+    dataset = pd.DataFrame()  # columns=[dataset_config['snapshot_features']["common"] + dataset_config['snapshot_features']["specific"]])
+
+    print(dataset)
+    new_col = []
+    for n, row in input_df.transpose().iterrows():  # transpose dataframe to iterate over columns
+        if row.name in common_features:
+            new_col = row.values
+            fill_common_features(common_features[row.name], dataset, new_col)
+        else:
+            pass
+            # - age
+            # - company_seniority
+            # - overall_experience
+            # - license_expiration
+            # - salary_6m_average
+            # - salary_current
+            # - time_since_salary_increase
+            # - income_6m_average
+            # - income_current
+            # - time_since_last_promotion
+            # - absenteeism_6m_average
+            # - absenteeism_2m_average
+            # - vacation_days  # except last month?
+            # - leader_left_3m
+            # - has_meal
+            # - has_insurance
+            # - penalties_2m
+            # - penalties_6m
+
+
+    print('result:', dataset)
+
     pass
 
 
 if __name__ == '__main__':
-    setup_path = 'data_config.yaml'
+    setup_path = '../data_config.yaml'
+    dataset_config_path = '../dataset_config.yaml'
     with open(setup_path) as stream:
-        config = yaml.load(stream, yaml.Loader)
-        for key, val in config.items():
-            print(val)
+        data_config = yaml.load(stream, yaml.Loader)
+        # for key, val in data_config.items():
+        #     print(val)
 
-    check_and_parse(config)
+    with open(dataset_config_path) as stream:
+        dataset_config = yaml.load(stream, yaml.Loader)
+        # print(dataset_config['snapshot_features']["common"])
+
+    check_and_parse(data_config, dataset_config)

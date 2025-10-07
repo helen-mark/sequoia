6186f90fcff8ee07141c820b66e3cf3afc89499d
elena <elena@elena-laptop>
2025-10-07 11:11:15 +0300

    add run_tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,3 @@
-import unittest
 from sequoia_tests import tests
 
 
@@ -6,3 +5,9 @@ if __name__ == '__main__':
     t = tests.TestMergeCsv()
     t.check_duplicates_test()
     t.handle_duplicates_test()
+
+    t = tests.TestSequoiaDataset()
+    t.setUp()
+    t.test_calc_numerical_average_basic()
+    t.test_process_timeseries_realistic_data()
+    t.test_extract_features_from_timeseries()
